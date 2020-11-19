694ba9e82cf0f95f914032d6613da01c5f33598a
e-lab innovations <33958126+e-labInnovations@users.noreply.github.com>
2020-11-19 19:28:29 +0530

    Update pwa-script.js
    FCM code also added

--- assets/pwa-script.js
+++ assets/pwa-script.js
@@ -1,3 +1,17 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyDrxtvPCMVHyVKsik8uvyIo64s28heyr3Q",
+    authDomain: "e-lab-innovations.firebaseapp.com",
+    databaseURL: "https://e-lab-innovations.firebaseio.com",
+    projectId: "e-lab-innovations",
+    storageBucket: "e-lab-innovations.appspot.com",
+    messagingSenderId: "185397314105",
+    appId: "1:185397314105:web:1fae2925feb2850a2e43e8"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+  
 if ("serviceWorker" in navigator) {
       if (navigator.serviceWorker.controller) {
         console.log("An active service worker found, no need to register");
@@ -32,6 +46,6 @@ if ("serviceWorker" in navigator) {
             console.log(error);
           });
       }
-    } else {
-        alert("Service worker not working")
-    }
+} else {
+      alert("Service worker not working")
+}
