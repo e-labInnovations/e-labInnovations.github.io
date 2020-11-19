efa4a13f4ddc46bdb6b78dda22b98ce00448f3a0
e-lab innovations <33958126+e-labInnovations@users.noreply.github.com>
2020-11-19 20:03:14 +0530

    Update pwa-script.js
    more functions are added

--- assets/pwa-script.js
+++ assets/pwa-script.js
@@ -10,42 +10,112 @@
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const messaging = firebase.messaging();
-  
-if ("serviceWorker" in navigator) {
-      if (navigator.serviceWorker.controller) {
-        console.log("An active service worker found, no need to register");
-      } else {
-        // Register the service worker
-        navigator.serviceWorker
-          .register("/serviceworker.js", {
-            scope: "./"
-          })
-          .then(function (reg) {
-            messaging.useServiceWorker(reg);
-            console.log("Service worker has been registered for scope: " + reg.scope);
-              
-            messaging
-              .requestPermission()
-              .then(() => {
-                //message.innerHTML = "Notifications allowed";
-                console.log("Notifications allowed");
-                return messaging.getToken();
-              })
-              .then(token => {
-                //tokenString.innerHTML = "Token Is : " + token;
-                console.log("Token Is : " + token);
-              })
-              .catch(err => {
-                //errorMessage.innerHTML = errorMessage.innerHTML + "; " + err;
-                console.log("No permission to send push", err);
-              });
-          })
-          .catch(error => {
-            console.log("Service worker error");
-            console.log(error);
-          });
-      }
-} else {
-      alert("Service worker not working")
+const messaging = firebase.messaging(),
+      pushBtn   = document.getElementById('push-button');
+
+let userToken    = null,
+    isSubscribed = false;
+
+window.addEventListener('load', () => {
+ if ("serviceWorker" in navigator) {
+       if (navigator.serviceWorker.controller) {
+         console.log("An active service worker found, no need to register");
+       } else {
+         // Register the service worker
+         navigator.serviceWorker
+           .register("/serviceworker.js", {
+             scope: "./"
+           })
+           .then(function (reg) {
+             messaging.useServiceWorker(reg)
+             initializePush()
+             console.log("Service worker has been registered for scope: " + reg.scope);
+           })
+           .catch(error => {
+             console.log("Service worker error");
+             console.log(error);
+           });
+       }
+ } else {
+       alert("Service worker not working")
+ }
+});
+
+
+function initializePush() {
+    userToken = localStorage.getItem('pushToken')
+
+    isSubscribed = userToken !== null
+    updateBtn()
+
+    pushBtn.addEventListener('click', () => {
+        pushBtn.disabled = true
+
+        if (isSubscribed) return unsubscribeUser()
+
+        return subscribeUser()
+    })
 }
+
+function updateBtn() {
+    if (Notification.permission === 'denied') {
+        pushBtn.textContent = 'Subscription blocked'
+        return
+    }
+
+    pushBtn.textContent = isSubscribed ? 'Unsubscribe' : 'Subscribe'
+    pushBtn.disabled = false
+}
+
+function subscribeUser() {
+    messaging.requestPermission()
+        .then(() => messaging.getToken())
+        .then(token => {
+
+            updateSubscriptionOnServer(token)
+            isSubscribed = true
+            userToken = token
+            localStorage.setItem('pushToken', token)
+            updateBtn()
+        })
+        .catch(err => console.log('Denied', err))
+}
+
+function updateSubscriptionOnServer(token) {
+    if (isSubscribed) {
+//         return database.ref('device_ids')
+//                 .equalTo(token)
+//                 .on('child_added', snapshot => snapshot.ref.remove())
+    }
+
+//     database.ref('device_ids').once('value')
+//         .then(snapshots => {
+//             let deviceExists = false
+
+//             snapshots.forEach(childSnapshot => {
+//                 if (childSnapshot.val() === token) {
+//                     deviceExists = true
+//                     return console.log('Device already registered.');
+//                 }
+
+//             })
+
+//             if (!deviceExists) {
+//                 console.log('Device subscribed');
+//                 return database.ref('device_ids').push(token)
+//             }
+//         })
+}
+
+function unsubscribeUser() {
+    messaging.deleteToken(userToken)
+        .then(() => {
+            updateSubscriptionOnServer(userToken)
+            isSubscribed = false
+            userToken = null
+            localStorage.removeItem('pushToken')
+            updateBtn()
+        })
+        .catch(err => console.log('Error unsubscribing', err))
+}
+
