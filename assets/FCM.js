b6072ffade816cd51fc94cec3621e90485f949a8
e-lab innovations <33958126+e-labInnovations@users.noreply.github.com>
2020-11-19 19:25:18 +0530

    Update FCM.js
    code removed

--- assets/FCM.js
+++ assets/FCM.js
@@ -1,18 +1,1 @@
-  const firebaseConfig = {
-    apiKey: "AIzaSyDrxtvPCMVHyVKsik8uvyIo64s28heyr3Q",
-    authDomain: "e-lab-innovations.firebaseapp.com",
-    databaseURL: "https://e-lab-innovations.firebaseio.com",
-    projectId: "e-lab-innovations",
-    storageBucket: "e-lab-innovations.appspot.com",
-    messagingSenderId: "185397314105",
-    appId: "1:185397314105:web:1fae2925feb2850a2e43e8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  const messaging = firebase.messaging();
-  
-  messaging.onMessage(payload => {
-  console.log("Message received. ", payload);
-  const { title, ...options } = payload.notification;
-});
+ 
